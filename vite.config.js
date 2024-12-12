dc58a7b37349a09384e88b0cb100d0c103ee1f87
Artem Grachov <grachov2195@gmail.com>
2024-12-13 00:56:10 +0200

    Added env variables

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import svgr from 'vite-plugin-svgr'
 import path from 'path';
 
 export default defineConfig({
-  root: path.resolve(__dirname, 'src'),
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
