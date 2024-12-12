311a289803cd7a4ad930beb4f3356493a0544b8d
Artem Grachov <grachov2195@gmail.com>
2024-12-13 00:27:53 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ComponentType } from 'react';
+
+const App: ComponentType = () => {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+    </div>
+  )
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr'
+import path from 'path';
+
+export default defineConfig({
+  root: path.resolve(__dirname, 'src'),
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  plugins: [
+    svgr({
+      svgrOptions: {
+        icon: true
+      },
+      esbuildOptions: { loader: 'tsx' }
+    }),
+  ],
+  build: {
+    outDir: path.resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+  },
+});
