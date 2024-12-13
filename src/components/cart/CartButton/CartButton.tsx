4c21e16b7913c7bfddf8ec4d436629d54d62fed2
Artem Grachov <grachov2195@gmail.com>
2024-12-13 05:23:49 +0200

    Cart button products counter

--- src/components/cart/CartButton/CartButton.tsx
+++ src/components/cart/CartButton/CartButton.tsx
@@ -3,15 +3,21 @@ import { ComponentType } from 'react';
 import { EModalKeys } from '@/constants/modals/modal-keys';
 
 import { useModalContext } from '@/providers/modals';
+import { useCartItemsCount } from '@/providers/cart/hooks/use-cart-items-count';
 
 import styles from './styles.module.scss';
 
 const CartButton: ComponentType = () => {
   const { openModal } = useModalContext();
+  const count = useCartItemsCount();
 
   const clickHandler = () => {
     openModal(EModalKeys.CART);
   }
+
+  const counterEl = count > 0 ? (
+    <span className={styles.counter}>{count}</span>
+  ) : null;
 
   return (
     <button
@@ -20,6 +26,7 @@ const CartButton: ComponentType = () => {
       onClick={clickHandler}
     >
       Cart
+      {counterEl}
     </button>
   )
 }
