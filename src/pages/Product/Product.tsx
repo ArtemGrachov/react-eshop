1936c2016c6fa369e9f804ddc8656a037c93f445
Artem Grachov <grachov2195@gmail.com>
2024-12-13 02:32:35 +0200

    Product details section

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -2,9 +2,11 @@ import { ComponentType } from 'react'
 
 import ProductDetails from './components/ProductDetails/ProductDetails'
 
+import styles from './styles.module.scss';
+
 const PageProduct: ComponentType = () => {
   return (
-    <main>
+    <main className={styles.page}>
       <ProductDetails />
     </main>
   )
--- src/pages/Product/components/ProductDetails/ProductDetails.tsx
+++ src/pages/Product/components/ProductDetails/ProductDetails.tsx
@@ -1,10 +1,29 @@
 import { ComponentType } from 'react';
 
+import styles from './styles.module.scss';
+import clsx from 'clsx';
+
 const ProductDetails: ComponentType = () => {
   return (
-    <div>
-      product details
-    </div>
+    <section>
+      <div className={styles.container}>
+        <div className={styles.row}>
+          <div className={clsx(styles.col, styles._sm)}>
+            <picture className={styles.imageWrap}>
+              <img src="" alt=""  className={styles.image} />
+            </picture>
+          </div>
+          <div className={clsx(styles.col, styles._lg)}>
+            <h2>
+              product
+            </h2>
+            <p>
+              price
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
